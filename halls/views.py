217cf0b5b6eb41c3c06071d896e123020356486b
MerryPoppins92 <merry2lassus@gmail.com>
2020-11-16 12:52:46 +0200

    contenu francais 5

--- halls/views.py
+++ halls/views.py
@@ -16,7 +16,7 @@ import requests
 YOUTUBE_API_KEY = 'AIzaSyBmj_BLIyzbXjGHV0GCcnbx9xkQtUvGQUk'
 
 def home(request):
-	recent_halls = Hall.objects.all().order_by('-id')[:2]
+	recent_halls = Hall.objects.all().order_by('-id')[:3]
 	popular_halls = [Hall.objects.get(pk = 1), Hall.objects.get(pk = 2), Hall.objects.get(pk = 5)]
 	return render(request, 'halls/home.html', {'recent_halls':recent_halls, 'popular_halls':popular_halls})
 
